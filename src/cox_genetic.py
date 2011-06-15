0234bd88cb99b2f4f395bc722b24855cf724b197
Jonas Kalderstam <jonask@thep.lu.se>
2011-06-15 14:35:07 +0200

    Really works with fake data, first genetic then cox shows consistent improvement. reaches 0.9 on noise data with just 100 epochs
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- src/cox_genetic.py
+++ src/cox_genetic.py
@@ -4,8 +4,10 @@ from kalderstam.neural.network import build_feedforward
 import numpy
 from survival.cox_error import orderscatter, get_C_index
 import survival.cox_error as cox_error
+from survival.cox_error import censor_rndtest
 import kalderstam.util.graphlogger as glogger
 import logging
+from kalderstam.neural.training.gradientdescent import traingd
 from kalderstam.neural.training.genetic import train_evolutionary
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
@@ -15,7 +17,12 @@ logger = logging.getLogger('kalderstam.neural.cox_training')
 def c_index_error(target, result):
     #len(target) first to compensate for internals in genetic training
     #abs( - 0.5) to make both "positive" and "negative" C_index work, since they do
-    return len(target) / abs(get_C_index(target, result) - 0.5) #return inverse, error should be low if c_index is high
+    C = get_C_index(target, result)
+    if C < 0.5:
+        #dont want these right now
+        C = 0.49 #this is bad
+
+    return len(target) / abs(C - 0.5) - 2 * len(target) #return inverse, error should be low if c_index is high. last minus term makes the minimum zero and not two.
 
 def plot_input(data):
     plt.figure()
@@ -50,7 +57,13 @@ def test(net, P, T, filename, epochs):
     try:
         #net = train_cox(net, (P, T), (None, None), timeslots, epochs, learning_rate = learning_rate)
         #net = traingd(net, (P, T), (None, None), epochs, learning_rate, block_size, error_module = cox_error)
-        net = train_evolutionary(net, (P, T), (None, None), epochs, error_function = c_index_error, population_size = 5)
+        net = train_evolutionary(net, (P, T), (None, None), epochs / 2, error_function = c_index_error, population_size = 50)
+
+        outputs = net.sim(P)
+        c_index = get_C_index(T, outputs)
+        logger.info("C index = " + str(c_index))
+
+        net = traingd(net, (P, T), (None, None), epochs, learning_rate = 1, block_size = 0, error_module = cox_error)
     except FloatingPointError:
         print('Aaawww....')
     outputs = net.sim(P)
@@ -65,21 +78,41 @@ if __name__ == "__main__":
     logging.basicConfig(level = logging.INFO)
     glogger.setLoggingLevel(glogger.debug)
 
+    #Real data
+
     filename = "/home/gibson/jonask/Dropbox/Ann-Survival-Phd/Two_thirds_of_SA_1889_dataset.txt"
-    #P, T = parse_file(filename, targetcols = [4, 5], inputcols = [2, -4, -3, -2, -1], ignorerows = [0], normalize = True)
     P, T = parse_file(filename, targetcols = [4, 5], inputcols = [2, -4, -3, -2, -1], ignorerows = [0], normalize = True)
 
     #remove tail censored
     P, T = copy_without_tailcensored(P, T)
 
+    #Fake data
+    #productfunction_wn = '/home/gibson/jonask/Dropbox/Ann-Survival-Phd/fake_data_set/productfunction_many_with_noise.txt'
+
+    #The training sample
+    #with_noise = productfunction_wn
+
+    #P, T_wn = parse_file(with_noise, targetcols = [4], inputcols = [0, 1, 2, 3], ignorecols = [], ignorerows = [], normalize = False)
+
+    #Amount to censor
+    #ratio = 0.50
+
+    #T_wn = censor_rndtest(T_wn, ratio)
+
+    #Training sample
+    #T = T_wn
+    #filename = with_noise
+
     #Limit to incourage overtraining!
-    #rows = sample(range(len(T)), 800)
+    #rows = sample(range(len(T)), 500)
     #P = P[rows]
     #T = T[rows]
 
+    #Network part
+
     p = len(P[0]) #number of input covariates
 
-    net = build_feedforward(p, 5, 1, output_function = 'linear')
+    net = build_feedforward(p, 10, 1, output_function = 'linear')
 
     #Initial state
     outputs = net.sim(P)
@@ -97,4 +130,5 @@ if __name__ == "__main__":
 
         outputs = net.sim(P)
         orderscatter(outputs, T, filename, 'o')
+        raw_input("Press enter to show plots...")
         glogger.show()
