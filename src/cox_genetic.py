7cd28bc17615188b5cc50f26b1bc2b063db55a86
Jonas Kalderstam <jonask@thep.lu.se>
2011-08-04 12:54:02 +0200

    Davis/version
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- src/cox_genetic.py
+++ src/cox_genetic.py
@@ -1,14 +1,15 @@
 from kalderstam.matlab.matlab_functions import plot_network_weights
 from kalderstam.util.filehandling import parse_file, load_network, save_network
-from kalderstam.neural.network import build_feedforward
+from kalderstam.neural.network import build_feedforward, \
+    build_feedforward_multilayered
 import numpy
 from survival.cox_error import orderscatter, get_C_index
 import survival.cox_error as cox_error
-from survival.cox_error import censor_rndtest
+from survival.cox_error import censor_rndtest, pre_loop_func, calc_sigma, calc_beta, cox_error as cerror
 import kalderstam.util.graphlogger as glogger
 import logging
 from kalderstam.neural.training.gradientdescent import traingd
-from kalderstam.neural.training.genetic import train_evolutionary
+from kalderstam.neural.training.davis_genetic import train_evolutionary
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from random import sample
@@ -23,6 +24,12 @@ def c_index_error(target, result):
         C = 0.49 #this is bad
 
     return len(target) / abs(C - 0.5) - 2 * len(target) #return inverse, error should be low if c_index is high. last minus term makes the minimum zero and not two.
+
+def beta_error(target, result):
+    vars = pre_loop_func(None, None, target, 0)
+    sigma = calc_sigma(result)
+    beta, beta_risk, part_func, weighted_avg = calc_beta(result, vars['timeslots'], vars['risk_groups'])
+    return len(result) * cerror(beta, sigma)
 
 def plot_input(data):
     plt.figure()
@@ -45,25 +52,24 @@ def copy_without_tailcensored(Porg, Torg, cutoff = 5):
 
     return P, T
 
-def test(net, P, T, filename, epochs):
+def test(net, P, T, filename, epochs, mutation_rate = 0.05):
     logger.info("Running genetic test for: " + filename + ' ' + str(epochs))
     print("Number of patients with events: " + str(T[:, 1].sum()))
     print("Number of censored patients: " + str((1 - T[:, 1]).sum()))
+
 
     outputs = net.sim(P)
     c_index = get_C_index(T, outputs)
     logger.info("C index = " + str(c_index))
 
     try:
-        #net = train_cox(net, (P, T), (None, None), timeslots, epochs, learning_rate = learning_rate)
-        #net = traingd(net, (P, T), (None, None), epochs, learning_rate, block_size, error_module = cox_error)
-        net = train_evolutionary(net, (P, T), (None, None), epochs / 2, error_function = c_index_error, population_size = 50)
+        net = train_evolutionary(net, (P, T), (None, None), epochs, error_function = c_index_error, population_size = 50, mutation_chance = mutation_rate)
 
         outputs = net.sim(P)
         c_index = get_C_index(T, outputs)
         logger.info("C index = " + str(c_index))
 
-        net = traingd(net, (P, T), (None, None), epochs, learning_rate = 1, block_size = 0, error_module = cox_error)
+        #net = traingd(net, (P, T), (None, None), epochs * 2, learning_rate = 1, block_size = 0, error_module = cox_error)
     except FloatingPointError:
         print('Aaawww....')
     outputs = net.sim(P)
@@ -78,13 +84,49 @@ if __name__ == "__main__":
     logging.basicConfig(level = logging.INFO)
     glogger.setLoggingLevel(glogger.debug)
 
-    #Real data
+    try:
+        mutation_rate = input('Please input a mutation rate (0.05): ')
+    except SyntaxError as e:
+        mutation_rate = 0.05
 
-    filename = "/home/gibson/jonask/Dropbox/Ann-Survival-Phd/Two_thirds_of_SA_1889_dataset.txt"
-    P, T = parse_file(filename, targetcols = [4, 5], inputcols = [2, -4, -3, -2, -1], ignorerows = [0], normalize = True)
+    SB22 = "/home/gibson/jonask/Dropbox/Ann-Survival-Phd/Two_thirds_of_SA_1889_dataset_SB22.txt"
+    Benmargskohorten = "/home/gibson/jonask/Dropbox/Ann-Survival-Phd/Two_thirds_of_SA_1889_dataset_Benmargskohorten.txt"
+    SB91b = "/home/gibson/jonask/Dropbox/Ann-Survival-Phd/Two_thirds_of_SA_1889_dataset_SB91b.txt"
+    all_studies = "/home/gibson/jonask/Dropbox/Ann-Survival-Phd/Two_thirds_of_SA_1889_dataset.txt"
+
+    #Real data
+    print("Studies to choose from:")
+    print("1: SB22")
+    print("2: Benmargskohorten")
+    print("3: SB91b")
+    print("0: All combined (default)")
+
+    study = input("Which study to train on?: ")
+    if study == 1:
+        filename = SB22
+    elif study == 2:
+        filename = Benmargskohorten
+    elif study == 3:
+        filename = SB91b
+    else:
+        filename = all_studies
+
+    try:
+        columns = input("Which columns to include? (Do NOT forget trailing comma if only one column is used, e.g. '3,'\nAvailable columns are: 2, -4, -3, -2, -1. Just press ENTER for all columns.\n")
+    except SyntaxError:
+        columns = (2, -4, -3, -2, -1)
+    #P, T = parse_file(filename, targetcols = [4, 5], inputcols = [2, -4, -3, -2, -1], ignorerows = [0], normalize = True)
+    P, T = parse_file(filename, targetcols = [4, 5], inputcols = columns, ignorerows = [0], normalize = True)
+
+    #Used for output comparison
+    studies = {}
+    studies[SB22] = parse_file(SB22, targetcols = [4, 5], inputcols = columns, ignorerows = [0], normalize = True)
+    studies[Benmargskohorten] = parse_file(Benmargskohorten, targetcols = [4, 5], inputcols = columns, ignorerows = [0], normalize = True)
+    studies[SB91b] = parse_file(SB91b, targetcols = [4, 5], inputcols = columns, ignorerows = [0], normalize = True)
+    studies[all_studies] = parse_file(all_studies, targetcols = [4, 5], inputcols = columns, ignorerows = [0], normalize = True)
 
     #remove tail censored
-    P, T = copy_without_tailcensored(P, T)
+    #P, T = copy_without_tailcensored(P, T)
 
     #Fake data
     #productfunction_wn = '/home/gibson/jonask/Dropbox/Ann-Survival-Phd/fake_data_set/productfunction_many_with_noise.txt'
@@ -112,23 +154,34 @@ if __name__ == "__main__":
 
     p = len(P[0]) #number of input covariates
 
-    net = build_feedforward(p, 10, 1, output_function = 'linear')
+    #net = build_feedforward(p, 10, 1, output_function = 'linear')
+    net = build_feedforward_multilayered(p, [7, 10], 1, output_function = 'linear')
 
     #Initial state
     outputs = net.sim(P)
     orderscatter(outputs, T, filename, 's')
 
     for var in xrange(len(P[0, :])):
-        plot_input(P[:, var])
+        try:
+            plot_input(P[:, var])
+        except FloatingPointError as e:
+            logger.error('Var ' + str(var) + ' failed plotting somehow...')
+            print(e)
 
     glogger.show()
 
-    epochs = 100
+    try:
+        epochs = input("Number of generations (200): ")
+    except SyntaxError as e:
+        epochs = 200
 
     for times in range(100):
-        net = test(net, P, T, filename, epochs)
+        #train
+        net = test(net, P, T, filename, epochs, mutation_rate = mutation_rate)
 
-        outputs = net.sim(P)
-        orderscatter(outputs, T, filename, 'o')
+        #show outputs on all different studies
+        for sfile, (ps, ts) in studies.items():
+            outputs = net.sim(ps)
+            orderscatter(outputs, ts, sfile, 'o')
         raw_input("Press enter to show plots...")
         glogger.show()
